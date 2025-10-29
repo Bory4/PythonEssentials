b18a2a7b8e54b5918bc8b1e38319ad673252b44e
Bory <krzysztof.borowka@o2.pl>
2025-10-29 16:43:42 +0100

    strings

---
+++ 3/strings.py
@@ -0,0 +1,28 @@
+# First example of escape characters /n /t
+print("Pierwsza licznia \n\tDruga linia z tabulacją")
+
+# Example of adding strings
+string1 = "Hello"
+string2 = "World"
+res = string1 + " " + string2
+
+# Playing with strings
+string = "Programowanie"
+print(string[0] + " " + string[-1] + " " + string[2])
+
+s = "Napis"
+print(len(s))
+
+print(string[1:5])
+
+info = "Uczymy się języka Python"
+print("Python" in info)
+
+s = "Python jest świetny"
+print(s.upper())
+
+s = "   'Python jest super'    "
+print(s.strip())
+
+s = "Uczę się Pythona"
+print(s.replace("Pythona", "Java"))
