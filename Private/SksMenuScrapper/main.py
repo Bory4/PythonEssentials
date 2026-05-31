16d84286b4b01ef64e074fca13621fb8498f3d61
Bory4 <krzysztof.borowka@o2.pl>
2026-05-31 21:43:38 +0200

    Adding Scrapper project

--- Private/SksMenuScrapper/main.py
+++ Private/SksMenuScrapper/main.py
@@ -11,5 +11,5 @@ for category in categories:
     print(f"Category: {category.find('h2').contents[0]}\n")
     for position in category.find_all("li"):
         content = position.contents
-        print(f"\t* {str(content[0]).strip().split(' ')[:-1]} Serving: {str(content[0]).strip().split(' ')[-1]} Price:{content[1].text} pln")
+        print(f"\t* {" ".join(str(content[0]).strip().split(' ')[:-1])}; Serving: {str(content[0]).strip().split(' ')[-1]}; Price:{content[1].text} pln")
     print()
